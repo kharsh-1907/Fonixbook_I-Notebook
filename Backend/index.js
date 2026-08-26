39846d8038ff53c101a0ec9340b79e642867c6f3
Harsh Khadde <khaddeharsh1907@gmail.com>
2026-08-26 09:01:35 +0530

    Use environment port for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 connectToMongo();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: 'http://localhost:3000',
